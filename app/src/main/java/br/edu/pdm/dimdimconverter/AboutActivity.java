4b59730e5062a423774dd1b0c1a0f0574d3f0bf8
Rômulo Augusto Oestreich Maciel Garcia <romulo.smo@gmail.com>
2016-09-11 00:23:44 -0300

    Implementing about activity.

--- app/src/main/java/br/edu/pdm/dimdimconverter/AboutActivity.java
+++ app/src/main/java/br/edu/pdm/dimdimconverter/AboutActivity.java
@@ -1,13 +1,43 @@
 package br.edu.pdm.dimdimconverter;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class AboutActivity extends AppCompatActivity {
+
+    private Button btnSair;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
+
+        btnSair = (Button) findViewById(R.id.btnSair);
+
+        btnSair.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                sair();
+            }
+        });
+    }
+
+    private void sair() {
+        AlertDialog.Builder dialogoSair = new AlertDialog.Builder(this);
+        dialogoSair.setTitle(R.string.dlgSair);
+        dialogoSair.setMessage(R.string.msgSair);
+        dialogoSair.setNegativeButton(R.string.opNao, null);
+        dialogoSair.setPositiveButton(R.string.opSim, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                finish();
+                System.exit(0);
+            }
+        });
+        dialogoSair.show();
     }
 }
